2729b127342230cbeea5c378552a6afcaeca4ccf
Tesla <230045@astanait.edu.kz>
2025-05-30 14:31:02 +0500

    smth

--- api-gateway/cmd/api-gateway/main.go
+++ api-gateway/cmd/api-gateway/main.go
@@ -174,7 +174,7 @@ func main() {
 	}
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:9999"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
